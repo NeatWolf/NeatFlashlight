9539d82c748835866469cb2f83ace70eb57a4fdb
NeatWolf <alessandro.salvati.github@gmail.com>
2024-05-25 11:09:25 +0100

    First commit

---
+++ NeatFlashlight/Patches/FlashlightPatch.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace NeatWolf.NeatFlashlight.Patches
+{
+    [HarmonyPatch(typeof(FlashlightItem))]
+    internal class FlashlightPatch
+    {
+        [HarmonyPatch(nameof(FlashlightItem.SwitchFlashlight))]
+        [HarmonyPostfix]
+        static void SwitchFlashlight(bool on, ref FlashlightItem __instance)
+        {
+            //__instance. .intensity = 2500f;
+        }
+    }
+}
---
+++ NeatFlashlight/Plugin.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace NeatWolf.NeatFlashlight
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "com.neatwolf.lc.NeatFlashlight";
+        public const string PLUGIN_NAME = "NeatFlashlight";
+        public const string PLUGIN_VERSION = "0.0.1";
+
+    }
+    
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class ModBase : BaseUnityPlugin
+    {
+        private readonly Harmony harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+        private static ModBase Instance;
+        private static ManualLogSource mls;
+
+        private static void LogInfo(string info)
+        {
+            if (mls == null)
+                mls = CreateLogger();
+            
+            mls.LogInfo(PluginInfo.PLUGIN_NAME+": "+info);
+        }
+
+        private static ManualLogSource CreateLogger()
+        {
+            mls = BepInEx.Logging.Logger.CreateLogSource(PluginInfo.PLUGIN_NAME);
+            return mls;
+        }
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+            
+            LogInfo("Awakened");
+            harmony.PatchAll(typeof(ModBase));
+        }
+    }
+}
